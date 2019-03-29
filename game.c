e56bb8b43eec6bc1946b52de154ec195e8d7d0ad
alvarxp <47323193+alvarxp@users.noreply.github.com>
2019-03-29 18:22:13 +0100

    Update game.c

--- game.c
+++ game.c
@@ -43,6 +43,7 @@ void game_callback_roll(Game* game);
 void game_callback_left(Game* game);
 void game_callback_right(Game* game);
 void game_callback_inspect(Game* game);
+void game_callback_move(Game *game);
 
 static callback_fn game_callback_fn_list[N_CALLBACK]={
   game_callback_unknown,
@@ -54,7 +55,8 @@ static callback_fn game_callback_fn_list[N_CALLBACK]={
   game_callback_roll,
   game_callback_left,
   game_callback_right,
-  game_callback_inspect};
+  game_callback_inspect,
+  game_callback_move};
 
 /**
    Implementación de la interfaz de juego
@@ -716,4 +718,41 @@ void game_callback_inspect(Game* game){
     }
   }
   return;
+}
+
+void game_callback_move(Game *game){
+  Link *link;
+
+  Id link_id = NO_ID;
+  Id next_space_id = NO_ID;
+  Id act_space_id = NO_ID;
+  char direction[MAX_STRING] = {0};
+
+  Player *player = game_get_player(game);
+
+  if (!game)
+      return;
+
+  strcpy(direction, command_get_id(game->last_cmd[0]));
+
+  if (!game || act_space_id == NO_ID)
+      return;
+
+  if (strcmp(direction, "north") == 0 || strcmp(direction, "n") == 0)
+    link_id = space_get_north(game_get_space(game, game_get_player_location(game)));
+
+  else if (strcmp(direction, "east") == 0 || strcmp(direction, "e") == 0)
+    link_id = space_get_east(game_get_space(game, game_get_player_location(game)));
+
+  else if (strcmp(direction, "south") == 0 || strcmp(direction, "s") == 0)
+    link_id = space_get_south(game_get_space(game, game_get_player_location(game)));
+
+  else if (strcmp(direction, "west") == 0 || strcmp(direction, "w") == 0)
+    link_id = space_get_west(game_get_space(game, game_get_player_location(game)));
+
+  else
+    return;
+
+  link = game_get_link(game, link_id);
+
 }
