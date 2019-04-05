d741548de55f40ae78714423f2a3ca254a4edd4a
alvarxp <47323193+alvarxp@users.noreply.github.com>
2019-04-05 17:09:21 +0200

    Add files via upload

---
+++ die_test.h
@@ -0,0 +1,20 @@
+/**
+ * @brief Se encarga de probar el funcionamiento del dado
+ *
+ * @file die_test.h
+ * @authors Álvaro Platón
+ * @version 2.0
+ * @date 04/04/2019
+ * @copyright GNU Public License
+ */
+
+#ifndef DIE_TEST_H
+#define DIE_TEST_H
+
+void test1_die_create();
+void test1_die_roll();
+void test2_die_roll();
+void test1_die_get_last_roll();
+void test2_die_get_last_roll();
+
+#endif
---
+++ object_test.h
@@ -0,0 +1,26 @@
+/**
+ * @brief Se encarga de probar el funcionamiento de los objetos
+ *
+ * @file object_test.h
+ * @authors Álvaro Platón
+ * @version 2.0
+ * @date 04/04/2019
+ * @copyright GNU Public License
+ */
+
+ #ifndef OBJECT_TEST_H
+#define OBJECT_TEST_H
+
+void test1_object_create();
+void test2_object_create();
+void test1_object_set_name();
+void test2_object_set_name();
+void test1_object_set_id();
+void test2_object_set_id();
+void test1_object_set_description();
+void test2_object_set_description();
+void test1_object_get_name();
+void test1_object_get_description();
+void test1_object_get_id();
+
+#endif
