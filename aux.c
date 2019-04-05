44aacf8b9eb18c6dc99497bb4e68dc8c46b34256
Nikephore <47322414+Nikephore@users.noreply.github.com>
2019-04-05 17:37:21 +0200

    Update aux.c

--- aux.c
+++ aux.c
@@ -2,3 +2,12 @@
     sprintf(str, " Description: %s", des);
     screen_area_puts(ge->descript, str);
   }
+
+char* game_get_description(Game* game){
+
+  if(game == NULL){
+    return NULL;
+  }
+
+  return game->gdesc;
+}
