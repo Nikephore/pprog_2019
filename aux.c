2bdd500bc70270ccfbfbebb86f93b03ece4cd3ac
Nikephore <47322414+Nikephore@users.noreply.github.com>
2019-04-05 17:40:12 +0200

    Update aux.c

--- aux.c
+++ aux.c
@@ -1,31 +1,18 @@
-void game_callback_inspect(Game* game){
-  int i = 0;
-  Id id_space = NO_ID, id_object = NO_ID;
+/**
+*@brief obtiene el número de objetos de un jugador
+*@param1 p. El jugador del que queremos obtener el nombre
+*@return devuelve el número de objetos del jugador.
+*/
+int player_get_num_objects(Player * p){
+  if(!p)
+    return -1;
 
-  if(!game){
-    return;
-  }
+  return inventory_get_num_objects(p->inventory);
+}
 
-  id_space = game_get_player_location(game);
+BOOL player_search_object(Player * p, Id id){
+  if(!p)
+    return FALSE;
 
-  if(strcmp(command_get_imput(game->last_cmd),"space") == 0 || strcmp(command_get_imput(game->last_cmd),"s") == 0){
-    return;
-  }
-
-  for(i=0; i<MAX_OBJECTS; i++){
-
-    if(player_get_inventory(game->player) == NULL || player_get_num_objects(game->player) < MAX_OBJECTS){
-      if(game->object[i] != NULL){
-        id_object = game_get_object_id(game, i);
-    /*Compara el nombre del objeto con el que se le pasa por el comando*/
-        if(strcmp(object_get_name(game->object[i]),command_get_imput(game->last_cmd)) == 0){
-          if (space_search_object(game_get_space(game, id_space), id_object) == TRUE || player_search_object(game->player, id_object) == TRUE){
-            strcpy(game->gdesc, object_get_description(game->object[i]));
-            return;
-          }
-        }
-      }
-    }
-  }
-  return;
+  return inventory_compare(p->inventory, id);
 }
