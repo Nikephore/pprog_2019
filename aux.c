11ec1c955cb543ac5949c0fe135236aae38789a7
Nikephore <47322414+Nikephore@users.noreply.github.com>
2019-04-05 17:38:22 +0200

    Update aux.c

--- aux.c
+++ aux.c
@@ -1,13 +1,31 @@
-  if(strcpy(des, game_get_description(game)) != NULL){
-    sprintf(str, " Description: %s", des);
-    screen_area_puts(ge->descript, str);
+void game_callback_inspect(Game* game){
+  int i = 0;
+  Id id_space = NO_ID, id_object = NO_ID;
+
+  if(!game){
+    return;
   }
 
-char* game_get_description(Game* game){
+  id_space = game_get_player_location(game);
 
-  if(game == NULL){
-    return NULL;
+  if(strcmp(command_get_imput(game->last_cmd),"space") == 0 || strcmp(command_get_imput(game->last_cmd),"s") == 0){
+    return;
   }
 
-  return game->gdesc;
+  for(i=0; i<MAX_OBJECTS; i++){
+
+    if(player_get_inventory(game->player) == NULL || player_get_num_objects(game->player) < MAX_OBJECTS){
+      if(game->object[i] != NULL){
+        id_object = game_get_object_id(game, i);
+    /*Compara el nombre del objeto con el que se le pasa por el comando*/
+        if(strcmp(object_get_name(game->object[i]),command_get_imput(game->last_cmd)) == 0){
+          if (space_search_object(game_get_space(game, id_space), id_object) == TRUE || player_search_object(game->player, id_object) == TRUE){
+            strcpy(game->gdesc, object_get_description(game->object[i]));
+            return;
+          }
+        }
+      }
+    }
+  }
+  return;
 }
