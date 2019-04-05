a21cda1a838acd16809f9a59e3f8e0f926f8f33f
Nikephore <47322414+Nikephore@users.noreply.github.com>
2019-04-05 17:42:43 +0200

    Update aux.c

--- aux.c
+++ aux.c
@@ -1,18 +1,15 @@
-/**
-*@brief obtiene el número de objetos de un jugador
-*@param1 p. El jugador del que queremos obtener el nombre
-*@return devuelve el número de objetos del jugador.
-*/
-int player_get_num_objects(Player * p){
-  if(!p)
-    return -1;
+BOOL inventory_compare(Inventory *inv, Id id){
+  int i;
 
-  return inventory_get_num_objects(p->inventory);
-}
-
-BOOL player_search_object(Player * p, Id id){
-  if(!p)
+  if(inv == NULL || id == NO_ID){
     return FALSE;
+  }
 
-  return inventory_compare(p->inventory, id);
+  for(i=0; i<inventory_get_num_objects(inv); i++){
+    if(inventory_get_object_id(inv, i) == id){
+      return TRUE;
+    }
+  }
+
+  return FALSE;
 }
