4b75b6d0ec260846c2d172a60eea89b1e2d657bc
alvarxp <47323193+alvarxp@users.noreply.github.com>
2019-04-08 13:34:43 +0200

    Update aux.c

--- aux.c
+++ aux.c
@@ -1,15 +1,36 @@
-BOOL set_compare(Set *set, Id id){
-  int i = 0;
+void game_callback_move(Game *game){
+  Link *link;
 
-  if(set == NULL || id == NO_ID){
-    return FALSE;
-  }
+  Id link_id = NO_ID;
+  Id next_space_id = NO_ID;
+  Id act_space_id = NO_ID;
+  char direction[MAX_STRING] = {0};
 
-  for(i=0; i < set->num_elem; i++){
-    if(set->array_id[i] == id){
-      return TRUE;
-    }
-  }
+  Player *player = game_get_player(game);
 
-  return FALSE;
+  if (!game)
+      return;
+
+  strcpy(direction, command_get_id(game->last_cmd[0]));
+
+  if (!game || act_space_id == NO_ID)
+      return;
+
+  if (strcmp(direction, "north") == 0 || strcmp(direction, "n") == 0)
+    link_id = space_get_north(game_get_space(game, game_get_player_location(game)));
+
+  else if (strcmp(direction, "east") == 0 || strcmp(direction, "e") == 0)
+    link_id = space_get_east(game_get_space(game, game_get_player_location(game)));
+
+  else if (strcmp(direction, "south") == 0 || strcmp(direction, "s") == 0)
+    link_id = space_get_south(game_get_space(game, game_get_player_location(game)));
+
+  else if (strcmp(direction, "west") == 0 || strcmp(direction, "w") == 0)
+    link_id = space_get_west(game_get_space(game, game_get_player_location(game)));
+
+  else
+    return;
+
+  link = game_get_link(game, link_id);
+
 }
